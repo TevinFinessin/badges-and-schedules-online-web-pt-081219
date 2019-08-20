048dad00cd040a93083f4ca367531e342c7cfb5e
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-20 20:13:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def rooms(speakers)
   batch_badge_creator(speakers).each do |badge|
     puts badge
 end
-assign_rooms(speakers)
+assign_rooms(speakers).each do
 
 #hash = Hash.new
 #%w(cat dog wombat).each_with_index { |item, index|
