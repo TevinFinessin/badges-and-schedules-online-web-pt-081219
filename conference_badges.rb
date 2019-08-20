45535a1fda62c53f17c2fd69f9d3e1a837b87c2b
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-20 20:17:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,3 +12,15 @@ def batch_badge_creator(attendees)
   end
   attendees_badges
 end
+def assign_rooms(speakers_lineup)
+ speakers_lineup.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(speakers)
+ batch_badge_creator(speakers).each do |badge|
+  puts badge
+ end
+ assign_rooms(speakers).each do |assignment|
+  puts assignment
+ end
+end
