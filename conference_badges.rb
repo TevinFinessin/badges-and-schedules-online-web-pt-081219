5fbf4a0fb8de3220f06267b1da4535cfa0fed760
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-20 19:54:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
+require "pry"
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees_badges = []
+  attendees.each do |badge|
+    attendees_badges << "Hello, my name is #{badge}."
+    #binding.pry
+  end
+  attendees_badges
+end
+
+def assign_rooms
+  
+end
+
+
+#hash = Hash.new
+#%w(cat dog wombat).each_with_index { |item, index|
+ # hash[item] = index
+#}
+#hash  
