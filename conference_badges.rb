cce8cdf7b894ff749dbdc3911ebc7fdae0fff9d1
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-20 20:16:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,20 +13,3 @@ def batch_badge_creator(attendees)
   attendees_badges
 end
 
-def assign_rooms(attendees_attending)
-  attendees_attending.each_with_index {|speaker,index| = "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
-end
-def rooms(speakers)
-  batch_badge_creator(speakers).each do |badge|
-    puts badge
-end
-def assign_rooms(speakers).each do |assignment|
-put assignment
-end
-end
-#hash = Hash.new
-#%w(cat dog wombat).each_with_index { |item, index|
- # hash[item] = index
-#}
-#hash
-end
